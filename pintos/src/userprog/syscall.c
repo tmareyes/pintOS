e69be16cb0f8f0729aef3e24a0f46b9c04e8fcb6
triciamarier <reyestr@hawaii.edu>
2021-09-28 15:52:26 -1000

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -148,24 +148,6 @@ syscall_handler (struct intr_frame *f ) //UNUSED)
       break;
     }
     default:
-    
-  printf ("system call!\n");
-  uint32_t *esp;
-  esp = f->esp;
-  printf ("am I here yet?");
-  switch (*esp) {
-    case SYS_EXIT:
-    {
-      printf("here1?");
-      sys_exit(-1);
-      break;
-    }
-    default:
-      {
-        printf("SHOULD NOT REACH HERE");
-        sys_exit(-1);
-        break;
-      }
   }
   thread_exit ();
 }
